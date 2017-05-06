008a23037bca0589f8e0f5f710120ec14d6f306c
Zleub <debray.arnaud@gmail.com>
2017-05-06 14:32:34 +0200

    automaton test

--- main.cpp
+++ main.cpp
@@ -12,13 +12,58 @@
 //       .+ydddddddddhs/.
 //           .-::::-`
 
-#include <AbstractVM.h>
+// #include <AbstractVM.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <deque>
+
+int is_equal(std::string a, std::string b) {
+	return a == b;
+}
+
+class Automaton {
+private:
+	/* data */
+
+public:
+	typedef int (*t_pred)(std::string, std::string);
+
+	class State {
+	public:
+		std::string l;
+		t_pred pred;
+
+		Automaton::State * left;
+		Automaton::State * right;
+		int operator()(std::string w) {
+			return pred(l, w);
+		};
+
+		State(std::string l, t_pred pred = is_equal) : l(l);
+		virtual ~State();
+	};
+
+	Automaton();
+	virtual ~Automaton ();
+};
+
+Automaton::Automaton() {};
+Automaton::~Automaton() {};
+
+Automaton::State::State(t_pred pred) : pred(pred) {}
+Automaton::State::~State() {}
 
 int main(int ac, char *av[])
 {
 	(void)ac;
 	(void)av;
-	AbstractVM avm(av[1]);
+	// AbstractVM avm(av[1]);
+	Automaton t;
+
+	Automaton::State A;
+	A.lex = "A";
+	A.left = new State()
 
 	return (0);
 }
